d0f57507300681fefacb263f690483e5be39e714
obf1313 <oubaifan@163.com>
2021-02-05 11:02:13 +0800

    加标题

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -4,10 +4,11 @@
  * @createTime: 2020/7/16 17:03
  **/
 import React, { useState } from 'react';
-import { Row, Layout, BackTop } from 'antd';
+import { Row, Layout } from 'antd';
 import { VerticalAlignTopOutlined } from '@ant-design/icons';
-import { Header } from '@components/index';
 import { useHistory } from 'react-router';
+// @ts-ignore
+import titlePrefix from '@static/images/titlePrefix.png';
 import './index.less';
 
 const { Content } = Layout;
@@ -28,6 +29,11 @@ const Home = (props: IProps) => {
   const [loading, setLoading] = useState<boolean>(false);
   return (
     <Row style={{ width: '100%' }}>
+      <div className="large-header">
+        <img src={titlePrefix} alt="标题前缀" height={20} style={{ marginRight: 20 }} />
+        中央加药
+        <img src={titlePrefix} alt="标题前缀" height={20} style={{ marginLeft: 20, transform: 'rotate(180deg)' }} />
+      </div>
       <Content className="content">
         {!loading && children}
       </Content>
